9c00280ecca0e8fcd367874cce0e98f2677446cf
pradeepiOS <pradeep.iostech@gmail.com>
2021-11-06 20:29:45 +0530

    Initial Commit

---
+++ SwiftUIFirebaseChat/SwiftUIFirebaseChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFirebaseChatApp.swift
+//  SwiftUIFirebaseChat
+//
+//  Created by Pradeep's Macbook on 06/11/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFirebaseChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
